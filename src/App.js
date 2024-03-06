3b835e6ece660fbed504ddf06bb82c8f61fa65f8
torCanHack <tor409617@gmail.com>
2024-03-06 21:58:45 +0100

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,66 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
-import {Prism as SyntaxHighLighter } from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { Provider } from 'react-redux';
-import { combineReducers } from 'redux';
-import { configureStore } from '@reduxjs/toolkit';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { okaidia } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import logo from './logo.svg';
 import './App.css';
-import { react } from '@babel/types';
 
-//A custom component to render code blocks with syntax highlighting
-const CodeBlock = ({ language, value}) => {
-  return <SyntaxHighLighter style={dark} language={language} children={value} />
-}
+// A custom component to render code blocks with syntax highlighting
+const CodeBlock = ({ language, children }) => {
+  return (
+    <SyntaxHighlighter style={okaidia} language={language}>
+      {children}
+    </SyntaxHighlighter>
+  );
+};
 
-//The main component that renders the editor and the preview
+// A custom component to render inline code
+const InlineCode = ({ children }) => {
+  return <code>{children}</code>;
+};
+
+// The main component that renders the editor and the preview
 const MarkdownEditor = () => {
-  //remember to come back and use redux to manage the state of the markdown text
+  const [markdown, setMarkdown] = useState(
+    `# Welcome to my React Markdown Previewer!
 
-  //for simplicity,I will use a local state here
-  const [markdown, setMarkdown] = React.useState ();
+    \`HelloWorld\`
+    
+This is a simple markdown editor built with React and Redux.
 
-  //A function to handle the change event of the textarea
+You can write your markdown text in the editor and see the preview below.
+
+## Features
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+- Live preview
+- Syntax highlighting
+- Responsive design
+
+## Example
+
+Here is some example code:
+
+\`\`\`javascript
+const add = (a, b) => a + b;
+console.log(add(2, 3));
+\`\`\`
+
+And here is some inline code: \`const multiply = (a, b) => a * b;\`
+
+![ilovefood](https://elements.envato.com/front-view-little-yummy-cake-with-lemon-slices-can-GT2AEDH)
+And here is the output:`
+  );
+
   const handleChange = (event) => {
-    //you need to disptach an action to update the Redux store
-    //For simplicity, I will use the local state here
-    setMarkdown(event.targer.value);
+    setMarkdown(event.target.value);
   };
 
-  return <section className='container'>
-
-    <section className='row'>
-
-      <div className='col-md-6'>
+  return (
+    <section className='container'>
+      <div className='editor'>
         <h1>Editor</h1>
-        <textarea id='editor' className='form-control' value={markdown} onChange={handleChange}/>
+        <textarea id='editor' value={markdown} onChange={handleChange}></textarea>
       </div>
 
-      <div className='col-md-6'>
+      <div>
         <h2>Preview</h2>
         <div id='preview' className='card'>
-          <div className='card-body'>
-            <ReactMarkdown children={markdown} components={{ code: CodeBlock }}/>
-          </div>
+          <ReactMarkdown components={{ code: CodeBlock, inlineCode: InlineCode }}>
+            {markdown}
+          </ReactMarkdown>
         </div>
       </div>
-
     </section>
-    
-  </section>
-}
-
-const rootReducer = combineReducers({
-  markdown: markdownReducer
-})
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <provider store={store}>
-        <MarkdownEditor />
-      </provider>
+    <div className='App'>
+      <MarkdownEditor />
     </div>
   );
 }
